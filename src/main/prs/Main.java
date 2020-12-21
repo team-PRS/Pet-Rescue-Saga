8af8d22254d739876d5a1ce1f6cd4011026358ee
raveneau <emilien.raveneau@gmail.com>
2020-12-21 09:16:41 +0100

    add of a JFrame for the game

--- src/main/prs/Main.java
+++ src/main/prs/Main.java
@@ -1,4 +1,5 @@
 package prs;
+import prs.graphics.Frame;
 
 import java.io.Serializable;
 import prs.map.*;
@@ -18,5 +19,7 @@ public class Main {
     System.out.println("==============================");
     map.getJoueur().getCompte().setScore(2,100);
     System.out.println(map);
+    System.out.println("==============================");
+    Frame f = new Frame();
   }
 }
---
+++ src/main/prs/graphics/Frame.java
@@ -0,0 +1,64 @@
+package prs.graphics;
+import javax.swing.JFrame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.Toolkit;
+
+public class Frame extends JFrame{
+
+  // CONSTRUCTORS --------------------------------------------------------------
+  public Frame(){
+    setTitle("Pet Rescue Saga");
+    //setLocationRelativeTo(null); // fenetre centrée
+    setExtendedState(JFrame.MAXIMIZED_BOTH);//Frame take as many space as it can.
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // bouton fermer par défaut.
+    //notre bouton fermé si dessous.
+    /*
+    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    Runnable r = new Runnable() {
+      @Override // indique au compilateur qu'on écrit sur la méthode run.
+      public void run() {
+        setBoutonFermer(); //Méthode qui demande validation si le joueur clic sur le bouton fermé la fenetre.
+      }
+    };*/
+    //fullScreen
+    /*
+    setUndecorated(true);
+    GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    gd.setFullScreenWindow(this);
+    */
+    setVisible(true);
+  }
+  // GET SET -------------------------------------------------------------------
+
+  // FUNCTIONS -----------------------------------------------------------------
+  public void setBoutonFermer(){
+    this.addWindowListener(new WindowAdapter() {
+        @Override // indique au compilateur qu'on écrit sur la méthode windowClosing déjà défini et il est sencé vérifier qu'on a pas fait de bêtise d'écriture.
+        public void windowClosing(WindowEvent e) {
+          quit();
+        }
+    });
+  }
+  public void quit(){
+    try {
+      boolean choix=true;
+      //ici on pourrait demander au joueur si il veux vraiment quitter le jeu
+      //si validation.
+      if(choix){
+        setVisible(false);
+        dispose();
+        // On provoque une fermeture normale du jeu.
+        //Main.quitter(); // on pourrait réaliser une sauvegarde automatique.
+        System.exit(0);
+      }//sinon rien.
+    }catch (Exception e) {
+      System.out.println("Someting wrong happend in Frame.quit()");
+      System.exit(1);
+    }
+  }
+  // SUB-CLASS -----------------------------------------------------------------
+
+}
