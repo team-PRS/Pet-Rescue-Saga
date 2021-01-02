4d93c14173da8d3df89a1a24033e12ef8fdc8a70
BRAGINA natalia <nata.br@gmail.com>
2021-01-02 02:01:51 +0100

    brouillon de la classe GUI

--- src/main/prs/GUIPrs.java
+++ src/main/prs/GUIPrs.java
@@ -8,6 +8,7 @@ public class GUIPrs
     private Jeu Motor;
     private PanelPlateau panelPlateau;
     private PanelGame panelGame;
+
     //et les autres variables de classes graphiques
 
     public GUIPrs()
@@ -15,33 +16,46 @@ public class GUIPrs
         Motor = new Jeu(true);
     }
 
+    /**
+     *  Save all accounts to file before closing
+     */
     public void GUIClose()
     {
-        this.Motor.Close();     //save account before closing
+        this.Motor.Close();  
     }
 
     public void windowPlay()
     {
         //window with button "play"
-        //click listener for button -> create accountWindow
+        //click listener for button -> create accountWindow  
     }
 
     public void accountWindow()
     {
         //window with menu deroulant:
-        //1) menu "new account" -> create registrationWindow()
-        //2) menu avec accounts existants -> getListOfJoueurs()
-        //                                   selectJoueur()
-        //                                  launchGame()  (va etre creer dans cette classe)
+        //1) menu "new account" -> create registrationWindow() va etre creer dans cette classe
+        //2) menu avec accounts existants -> getListOfJoueurs() de la classe Jeu
+        //                                   selectJoueur() de la classe Jeu
+        //                                   createPanelGame() va etre creer dans cette classe
+        //                                  launchGame()  va etre creer dans cette classe
     }
 
     public void registrationWindow()
     {
         //window with:
         // 1) pannel with text "Invent your pseudo:"
-        // 2) feunetre ou enter the pseudo
-        // 3) button "enter" -> takes String from 2) + createNewJoueu()
+        // 2) feunetre ou taper String pseudo
+        // 3) button "enter" -> takes String from 2) + createNewJoueur()
 
+    }
+
+    public void createPanelGame()
+    {
+        //create window PanelGame
+        //create panelPlateau - y passer les params de getPlateauWidth(), getPlateauHeight(), getCell()
+        //create panelInfo (y passer les params de Motor.getCurrentJoueur().getCompte() ets)
+        //bouton "exit" or prevenir faire GUIClose() quand on ferme window PanelGame -
+        //                                -  save all gamers to file before closing window
     }
 
     public void launchGame()
